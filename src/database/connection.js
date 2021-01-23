8bd01d1a03b574419a650ac649a449dc25a8faa2
silogecho97 <silogecho@gmail.com>
2021-01-23 10:01:00 +0300

    production db

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,11 +1,23 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("chatdb", "root", "password", {
-  host: "192.168.0.10",
+// const sequelize = new Sequelize("chatdb", "root", "password", {
+//   host: "192.168.0.10",
+//   dialect: "mysql",
+//   timezone: '+03:00',
+//   logging:console.log,
+//   pool: {
+//     max: 500,
+//     min: 30,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// });
+
+const sequelize = new Sequelize("chatdb", "root", "Til&925!Tek", {
+  host: "localhost",
   dialect: "mysql",
   logging: false,
   timezone: '+03:00',
-  logging:console.log,
   pool: {
     max: 500,
     min: 30,
